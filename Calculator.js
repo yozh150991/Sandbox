8f8e4ccb836137ba2589df01fe08ebda1f105558
Igor Glushko <igorglushko150991@gmail.com>
2020-03-20 23:31:01 +0200

    Added Card animation, yesterday's button and started work on calculator

---
+++ Calculator.js
@@ -0,0 +1,18 @@
+const time = 10 000; //ms
+const step = 5;
+
+function outNum(num, elem) {
+    let a = document.querySelector("#" + elem);
+    n = 0;
+    let t = Math.round(time/(num/step));
+    let interval = setInterval(() => {
+        n = n + step;
+        if (n == num) {
+            clearInterval(interval);
+        }
+        a.innerHTML = n;
+    },
+    t);
+}
+
+outNum(1000, "out-1");
