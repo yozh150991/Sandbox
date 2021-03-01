5e4e2f8aa5e950ec6d7f768a1eeb5870862689e4
Igor Glushko <igorglushko150991@gmail.com>
2021-03-02 00:08:22 +0200

    GameDEv training

---
+++ Game_Dev/js/play_game.js
@@ -0,0 +1,10 @@
+function draw() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    // Draw slice
+    /*ctx.drawImage(document.getElementById('source'),
+                  33, 71, 104, 124, 21, 20, 87, 104);
+  
+    // Draw frame
+    ctx.drawImage(document.getElementById('frame'), 0, 0);*/
+}
