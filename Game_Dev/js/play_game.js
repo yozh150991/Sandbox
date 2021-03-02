a686caa398397c154025b843c805e30938647fc7
yozh150991 <dragonelf1509@gmail.com>
2021-03-02 12:05:36 +0200

    worked on draw image

--- Game_Dev/js/play_game.js
+++ Game_Dev/js/play_game.js
@@ -1,10 +1,28 @@
+let imgArr = [];
+let drawnArr = [];
+function pushImages (){
+    for (let i=0; i<document.getElementsByClassName('front').length; i++){
+        imgArr.push(document.getElementsByClassName('front')[i]);
+    }
+}
+pushImages();
+
 function draw() {
     var ctx = document.getElementById('canvas').getContext('2d');
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    // Draw slice
-    /*ctx.drawImage(document.getElementById('source'),
-                  33, 71, 104, 124, 21, 20, 87, 104);
-  
-    // Draw frame
-    ctx.drawImage(document.getElementById('frame'), 0, 0);*/
+    // Draw image
+    for (let i=0; i<4; i++){
+        for (let j=0; j<4; j++){
+            let x = 25 + j * 200; 
+            let y = 25 + i * 300;
+            let randomIndex = Math.floor(Math.random()*imgArr.length);
+            if (drawnArr.includes(randomIndex)){
+                randomIndex = Math.floor(Math.random()*imgArr.length);
+            }
+            else{
+                drawnArr.push(randomIndex);
+                ctx.drawImage(imgArr[randomIndex], x, y, 200, 300);
+            }
+        }
+    }
 }
