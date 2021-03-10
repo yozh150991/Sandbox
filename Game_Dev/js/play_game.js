db5f80aa93a2ca220f327b0781a3dd41091841aa
Igor Glushko <igorglushko150991@gmail.com>
2021-03-10 23:54:53 +0200

    added array of front cards

--- Game_Dev/js/play_game.js
+++ Game_Dev/js/play_game.js
@@ -1,13 +1,34 @@
 let imgArr = [];
-let drawnArr = [];
+let imgArrSecond = [];
+let imgArrFinal = [];
+function shuffle(arr) {
+    var j, x, i;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
+    }
+    return arr;
+}
+
 function pushImages (){
     for (let i=0; i<document.getElementsByClassName('front').length; i++){
         imgArr.push(document.getElementsByClassName('front')[i]);
     }
+    shuffle(imgArr);
 }
 pushImages();
+imgArrSecond = shuffle(imgArr);
+imgArrFinal = imgArr.concat(imgArrSecond);
 
-function draw() {
+/*function drawFront(elem) {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    // Draw image
+            ctx.drawImage(document.getElementById('front'), x, y, 100, 150);
+        }*/
+
+function drawBack() {
     var ctx = document.getElementById('canvas').getContext('2d');
     ctx.strokeRect(0, 0, canvas.width, canvas.height);
     // Draw image
@@ -15,14 +36,6 @@ function draw() {
         for (let j=0; j<4; j++){
             let x = 25 + j * 110; 
             let y = 25 + i * 175;
-            /*let randomIndex = Math.floor(Math.random()*imgArr.length);
-            if (drawnArr.includes(randomIndex)){
-                randomIndex = Math.floor(Math.random()*imgArr.length);
-            }
-            else{
-                drawnArr.push(randomIndex);
-                ctx.drawImage(imgArr[randomIndex], x, y, 200, 300);
-            }*/
             ctx.drawImage(document.getElementById('back'), x, y, 100, 150);
         }
     }
@@ -31,5 +44,5 @@ let button = document.getElementById('button');
         button.onclick = function(){
             document.getElementById('canvas').classList.toggle('active');
             button.hidden = true;
-            draw();
+            drawBack();
         };
