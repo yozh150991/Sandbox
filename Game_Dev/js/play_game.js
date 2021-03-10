5444517833d9a868fc36aa32f15223b14d9dcb2b
yozh150991 <dragonelf1509@gmail.com>
2021-03-10 09:28:18 +0200

    Back cards are live

--- Game_Dev/js/play_game.js
+++ Game_Dev/js/play_game.js
@@ -13,16 +13,23 @@ function draw() {
     // Draw image
     for (let i=0; i<4; i++){
         for (let j=0; j<4; j++){
-            let x = 25 + j * 200; 
-            let y = 25 + i * 300;
-            let randomIndex = Math.floor(Math.random()*imgArr.length);
+            let x = 25 + j * 110; 
+            let y = 25 + i * 175;
+            /*let randomIndex = Math.floor(Math.random()*imgArr.length);
             if (drawnArr.includes(randomIndex)){
                 randomIndex = Math.floor(Math.random()*imgArr.length);
             }
             else{
                 drawnArr.push(randomIndex);
                 ctx.drawImage(imgArr[randomIndex], x, y, 200, 300);
-            }
+            }*/
+            ctx.drawImage(document.getElementById('back'), x, y, 100, 150);
         }
     }
 }
+let button = document.getElementById('button');
+        button.onclick = function(){
+            document.getElementById('canvas').classList.toggle('active');
+            button.hidden = true;
+            draw();
+        };
