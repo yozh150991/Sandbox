26f7f277b06e09fd42190eb83e772816fd854b3e
yozh150991 <dragonelf1509@gmail.com>
2021-05-30 23:25:53 +0300

    work on

--- Game_Dev/Tank_game_pixi/Classes/Game.js
+++ Game_Dev/Tank_game_pixi/Classes/Game.js
@@ -24,10 +24,72 @@ class Loading extends State{
         return "Loading";
     }
     draw(){
-        /*Pixi loader here
+        PIXI.loader
+        .add('loader-bg',  "assets/loader_bar/loader-bg.png")
+        .add('loader-bar',  "assets/loader_bar/loader-bar.png")
+        .load(setup);
+
         this.view = new Container();
         app.stage.addChild(this.view);
-        проверка отрисована сцена или нет
+        i
+
+        function setup() {
+            if (this.view.visible){
+                break
+            }
+            else {
+                this.setLoaderBar();
+                PIXI.loader
+                .add('eagle',  "assets/board/eagle.png")
+                .add('leaves',  "assets/board/leaves.png")
+                .add('wall_1',  "assets/board/small_wall_1.png")
+                .add('wall_2',  "assets/board/small_wall_2.png")
+                .add('wall_3',  "assets/board/small_wall_3.png")
+                .add('wall_4',  "assets/board/small_wall_4.png")
+                .add('wall',  "assets/board/wall.png")
+                .add('water',  "assets/board/water.png")
+                .add('enemy_blue',  "assets/tanks/enemy_blue.png")
+                .add('enemy_red',  "assets/tanks/enemy_red.png")
+                .add('enemy_white',  "assets/tanks/enemy_white.png")
+                .add('tank',  "assets/tanks/tank.png")
+                .add('bonus_immortal',  "assets/bonus/bonus_immortal.png")
+                .add('bonus_live',  "assets/bonus/bonus_live.png")
+                .add('bonus_slow',  "assets/bonus/bonus_slow.png")
+                .add('bonus_speed',  "assets/bonus/bonus_speed.png")
+                .add('appear',  "assets/appear.png")
+                .add('bullet',  "assets/bullet.png")
+                .add('button',  "assets/button.png")
+                .add('enemy_bullet',  "assets/enemy_bullet.png")
+                .add('explode_small',  "assets/explode_small.png")
+                .add('explode',  "assets/explode.png")
+                .add('scores',  "assets/scores.png")
+                .add('bonus_sound',  "assets/sounds/bonus.wav")
+                .add('explode_sound',  "assets/sounds/explode.wav")
+                .add('hit_sound',  "assets/sounds/hit.wav")
+                .add('lose_sound',  "assets/sounds/lose.wav")
+                .add('shot_sound',  "assets/sounds/shot.wav")
+                .add('win_sound',  "assets/sounds/win.wav")
+                .on("progress", progressBar)
+                .load(change);
+
+                function progressBar(){
+                    
+                }
+            }
+        }
+
+        function setLoaderBar(){
+            this.loaderBg = new Sprite(PIXI.loader.resources["loader-bg"].texture);
+            this.loaderBg.position.set(app.width/2, app.width/2 + 100);
+            this.loaderBg.anchor.setTo(0.5);
+            this.loaderBar = new Sprite(PIXI.loader.resources["loader-bar"].texture);
+            this.loaderBg.position.set(app.width/2, this.loaderBg.y);
+            this.loaderBar.anchor.setTo(0.5);
+            this.view.addChild(this.loaderBg);
+            this.view.addChild(this.loaderBar);
+            return loaderBar = this.loaderBar;
+        }
+        /*проверка отрисована сцена или нет
         лоадинг бар маленький лучше сохранить как локальную переменную диз чтото там с отсылкой на контейнер 
         */
     }
@@ -99,4 +161,4 @@ class Game{
 }
 
 let game = new Game();
-console.log(game.sign());
+window.onload = game;
--- Game_Dev/Tank_game_pixi/game.js
+++ Game_Dev/Tank_game_pixi/game.js
@@ -9,40 +9,7 @@ let Application = PIXI.Application,
 
 let app = new Application({width: 1024, height: 768});
 document.body.appendChild(app.view);
-
-PIXI.loader
-    .add(["assets/loader_bar/loader-bg.png",
-          "assets/loader_bar/loader-bar.png",  
-          "assets/board/eagle.png",
-          "assets/board/leaves.png",
-          "assets/board/small_wall_1.png",
-          "assets/board/small_wall_2.png",
-          "assets/board/small_wall_3.png",
-          "assets/board/small_wall_4.png",
-          "assets/board/wall.png",
-          "assets/board/water.png",
-          "assets/tanks/enemy_blue.png",
-          "assets/tanks/enemy_red.png",
-          "assets/tanks/enemy_white.png",
-          "assets/tanks/tank.png",
-          "assets/bonus/bonus_immortal.png",
-          "assets/bonus/bonus_live.png",
-          "assets/bonus/bonus_slow.png",
-          "assets/bonus/bonus_speed.png",
-          "assets/appear.png",
-          "assets/bullet.png",
-          "assets/button.png",
-          "assets/enemy_bullet.png",
-          "assets/explode_small.png",
-          "assets/explode.png",
-          "assets/scores.png",
-          "assets/sounds/bonus.wav",
-          "assets/sounds/explode.wav",
-          "assets/sounds/hit.wav",
-          "assets/sounds/lose.wav",
-          "assets/sounds/shot.wav",
-          "assets/sounds/win.wav",
-  ])
+pixi
     .on("progress", loadProgressHandler)
     .load(setup);
 
