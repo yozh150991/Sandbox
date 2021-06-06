4ef7bae61baea24092cc91c1cb7e7f07d164e063
yozh150991 <dragonelf1509@gmail.com>
2021-06-06 22:43:45 +0300

    teee

--- Game_Dev/Tank_game_pixi/game.js
+++ Game_Dev/Tank_game_pixi/game.js
@@ -21,9 +21,10 @@ class State {
     }
 }
 
-class Loading extends State{
+
+class Init extends State{
     constructor(){
-        super('loading', Menu);
+        super('init', Loading);
     }
     draw(){
         this.view = new Container();
@@ -43,7 +44,31 @@ class Loading extends State{
             }
             else {
                 this.setLoaderBar();
-                PIXI.loader
+                ()=>{game.nextState};
+            }
+        }
+
+        function setLoaderBar(){
+            this.loaderBg = new Sprite(PIXI.loader.resources["loader-bg"].texture);
+            this.loaderBg.position.set(app.width/2, app.width/2 + 100);
+            this.loaderBg.anchor.setTo(0.5);
+            this.loaderBar = new Sprite(PIXI.loader.resources["loader-bar"].texture);
+            this.loaderBg.position.set(app.width/2, this.loaderBg.y);
+            this.loaderBar.anchor.setTo(0.5);
+            this.view.addChild(this.loaderBg);
+            this.view.addChild(this.loaderBar);
+        }
+    }
+}
+class Loading extends State{
+    constructor(){
+        super('loading', Menu);
+    }
+    draw(){
+        this.view = new Container();
+        app.stage.addChild(this.view);
+
+        PIXI.loader
                 .add('eagle',  "assets/board/eagle.png")
                 .add('leaves',  "assets/board/leaves.png")
                 .add('wall_1',  "assets/board/small_wall_1.png")
@@ -74,25 +99,22 @@ class Loading extends State{
                 .add('shot_sound',  "assets/sounds/shot.wav")
                 .add('win_sound',  "assets/sounds/win.wav")
                 .on("progress", progressBar)
-                .load(
-                    ()=>{game.nextState}
-                );
+                .load(setup.bind(this));
 
                 function progressBar(loader){
                     loader.width = loaderBar.width*(loader.progress/100);
                 }
-            }
-        }
 
-        function setLoaderBar(){
-            this.loaderBg = new Sprite(PIXI.loader.resources["loader-bg"].texture);
-            this.loaderBg.position.set(app.width/2, app.width/2 + 100);
-            this.loaderBg.anchor.setTo(0.5);
-            this.loaderBar = new Sprite(PIXI.loader.resources["loader-bar"].texture);
-            this.loaderBg.position.set(app.width/2, this.loaderBg.y);
-            this.loaderBar.anchor.setTo(0.5);
-            this.view.addChild(this.loaderBg);
-            this.view.addChild(this.loaderBar);
+        this.view.visible = true;
+
+
+        function setup() {
+            if (this.view.visible){
+                return
+            }
+            else {
+                ()=>{game.nextState}
+            }
         }
     }
 }
@@ -102,10 +124,57 @@ class Menu extends State{
         super('Menu', EndGame);
     }
     draw(){
-        /*Pixi loader here
-        container = new Container();
-        app.stage.addChild(container);
-        */
+        this.view = new Container();
+        app.stage.addChild(this.view);
+
+        PIXI.loader
+                .add('eagle',  "assets/board/eagle.png")
+                .add('leaves',  "assets/board/leaves.png")
+                .add('wall_1',  "assets/board/small_wall_1.png")
+                .add('wall_2',  "assets/board/small_wall_2.png")
+                .add('wall_3',  "assets/board/small_wall_3.png")
+                .add('wall_4',  "assets/board/small_wall_4.png")
+                .add('wall',  "assets/board/wall.png")
+                .add('water',  "assets/board/water.png")
+                .add('enemy_blue',  "assets/tanks/enemy_blue.png")
+                .add('enemy_red',  "assets/tanks/enemy_red.png")
+                .add('enemy_white',  "assets/tanks/enemy_white.png")
+                .add('tank',  "assets/tanks/tank.png")
+                .add('bonus_immortal',  "assets/bonus/bonus_immortal.png")
+                .add('bonus_live',  "assets/bonus/bonus_live.png")
+                .add('bonus_slow',  "assets/bonus/bonus_slow.png")
+                .add('bonus_speed',  "assets/bonus/bonus_speed.png")
+                .add('appear',  "assets/appear.png")
+                .add('bullet',  "assets/bullet.png")
+                .add('button',  "assets/button.png")
+                .add('enemy_bullet',  "assets/enemy_bullet.png")
+                .add('explode_small',  "assets/explode_small.png")
+                .add('explode',  "assets/explode.png")
+                .add('scores',  "assets/scores.png")
+                .add('bonus_sound',  "assets/sounds/bonus.wav")
+                .add('explode_sound',  "assets/sounds/explode.wav")
+                .add('hit_sound',  "assets/sounds/hit.wav")
+                .add('lose_sound',  "assets/sounds/lose.wav")
+                .add('shot_sound',  "assets/sounds/shot.wav")
+                .add('win_sound',  "assets/sounds/win.wav")
+                .on("progress", progressBar)
+                .load(setup.bind(this));
+
+                function progressBar(loader){
+                    loader.width = loaderBar.width*(loader.progress/100);
+                }
+
+        this.view.visible = true;
+
+
+        function setup() {
+            if (this.view.visible){
+                return
+            }
+            else {
+                ()=>{game.nextState}
+            }
+        }
     }
 }
 
@@ -140,7 +209,7 @@ class Game{
         }
         Game.instance = this;
         Game.exists = true;
-        this.state = new Loading();
+        this.state = new Init();
         this.state.draw();
     }
       nextState() {
