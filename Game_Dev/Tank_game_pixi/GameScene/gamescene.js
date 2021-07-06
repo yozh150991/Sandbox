56ffa93a0f92053560cfe4ab45633ede3d7ba123
yozh150991 <dragonelf1509@gmail.com>
2021-07-06 18:42:09 +0300

    12

--- Game_Dev/Tank_game_pixi/GameScene/gamescene.js
+++ Game_Dev/Tank_game_pixi/GameScene/gamescene.js
@@ -23,28 +23,41 @@ export class Battlefield {
 
                 }
                 if (cellName === '2'){
-                    this.cell.ul = new Sprite(loader.resources["assets/board/small_wall_1.png"].texture);
-                    this.cell.ul.height = cellSize/2;
-                    this.cell.ul.width = cellSize/2;
-                    this.cell.ul.position.set(cellSize * j, cellSize * i)
-                    this.view.addChild(this.cell.ul)
-                    this.cell.ur = new Sprite(loader.resources["assets/board/small_wall_2.png"].texture);
-                    this.cell.ur.height = cellSize/2;
-                    this.cell.ur.width = cellSize/2;
-                    this.cell.ur.position.set(cellSize * j + cellSize/2, cellSize * i)
-                    this.view.addChild(this.cell.ur)
-                    this.cell.ll = new Sprite(loader.resources["assets/board/small_wall_3.png"].texture);
-                    this.cell.ll.height = cellSize/2;
-                    this.cell.ll.width = cellSize/2;
-                    this.cell.ll.position.set(cellSize * j, cellSize * i + cellSize/2)
-                    this.view.addChild(this.cell.ll)
-                    this.cell.lr = new Sprite(loader.resources["assets/board/small_wall_4.png"].texture);
-                    this.cell.lr.height = cellSize/2;
-                    this.cell.lr.width = cellSize/2;
-                    this.cell.lr.position.set(cellSize * j + cellSize/2, cellSize * i + cellSize/2)
-                    this.view.addChild(this.cell.lr)
+                    this.cell_ul = new Sprite(loader.resources["assets/board/small_wall_1.png"].texture);
+                    this.cell_ul.height = cellSize/2;
+                    this.cell_ul.width = cellSize/2;
+                    this.cell_ul.position.set(cellSize * j, cellSize * i)
+                    this.view.addChild(this.cell_ul)
+                    this.cell_ur = new Sprite(loader.resources["assets/board/small_wall_2.png"].texture);
+                    this.cell_ur.height = cellSize/2;
+                    this.cell_ur.width = cellSize/2;
+                    this.cell_ur.position.set(cellSize * j + cellSize/2, cellSize * i)
+                    this.view.addChild(this.cell_ur)
+                    this.cell_ll = new Sprite(loader.resources["assets/board/small_wall_3.png"].texture);
+                    this.cell_ll.height = cellSize/2;
+                    this.cell_ll.width = cellSize/2;
+                    this.cell_ll.position.set(cellSize * j, cellSize * i + cellSize/2)
+                    this.view.addChild(this.cell_ll)
+                    this.cell_lr = new Sprite(loader.resources["assets/board/small_wall_4.png"].texture);
+                    this.cell_lr.height = cellSize/2;
+                    this.cell_lr.width = cellSize/2;
+                    this.cell_lr.position.set(cellSize * j + cellSize/2, cellSize * i + cellSize/2)
+                    this.view.addChild(this.cell_lr)
                 }
-
+                /*if (cellName === '6'){
+                    this.cell = new Sprite(loader.resources["assets/board/wall.png"].texture);
+                    this.cell.position.set(cellSize * j, cellSize * i);
+                    this.cell.height = cellSize;
+                    this.cell.width = cellSize;
+                    this.view.addChild(this.cell);
+                }*/
+                if (cellName === '7'){
+                    this.cell = new Sprite(loader.resources["assets/board/eagle.png"].texture);
+                    this.cell.position.set(cellSize * j, cellSize * i);
+                    this.cell.height = cellSize;
+                    this.cell.width = cellSize;
+                    this.view.addChild(this.cell);
+                }
               }
             }
           }
