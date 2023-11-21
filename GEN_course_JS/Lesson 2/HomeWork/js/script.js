ca082d5532731cca3b396f9f22215edf78835a43
yozh150991 <dragonelf1509@gmail.com>
2023-11-22 00:18:18 +0200

    update

---
+++ GEN_course_JS/Lesson 2/HomeWork/js/script.js
@@ -0,0 +1,88 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// let value = prompt("Input value");
+// if (parseInt(value) > 0 ){
+//     console.log(true);
+// }
+// else {
+//     console.log(false)
+// };
+// if (value === "test"){
+//         console.log(true);
+//     }
+//     else {
+//         console.log(false)
+//     };
+// if (parseInt(value) > 10){
+//     console.log(parseInt(value) - 5);
+// }
+// else {
+//     console.log(parseInt(value) + 5)
+// };
+// switch (value) {
+// case "1":
+//     alert(`January`);
+//     break;
+// case "2":
+//     alert(`February`);
+//     break;
+// case "3":
+//     alert(`March`);
+//     break;
+// case "4":
+//     alert(`April`);
+//     break;
+//     case "5":
+//         alert(`May`);
+//         break;
+//     case "6":
+//         alert(`June`);
+//         break;
+//     case "7":
+//         alert(`July`);
+//         break;
+//     case "8":
+//         alert(`August`);
+//         break;
+//         case "9":
+//             alert(`September`);
+//             break;
+//         case "10":
+//             alert(`October`);
+//             break;
+//         case "11":
+//             alert(`November`);
+//             break;
+//         case "12":
+//             alert(`December`);
+//             break;
+
+// default:
+//     alert(`Incorrect input`);
+// }
+// let a = parseInt(value);
+// let sum = 0;
+// if (a > 99 && a < 1000){
+//     while (a > 0) {
+//         sum += a % 10
+//         a = Math.floor(a/10)
+//         console.log(a);
+//         console.log(sum);12
+//     }
+//     alert(sum);
+
+// }
